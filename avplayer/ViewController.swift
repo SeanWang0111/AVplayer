0785980a567c83225f4eca22781386ddc10b6473
Sean Wang <seanwang0111@gmail.com>
2021-03-18 10:30:57 +0800

    Initial Commit

---
+++ avplayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avplayer
+//
+//  Created by Sean Wang on 2021/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
